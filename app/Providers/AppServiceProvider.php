0fff904ca1c1c4a8985630808ac845961d77e430
hiro0611 <life.for.study.eco.his@gmail.com>
2020-11-17 13:23:54 +0900

    fixed varchar config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,10 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //Schema::defaultStringLength(191);
-        //if (\App::environment('production')) {
-        //\URL::forceScheme('https');
-        //}
+        Schema::defaultStringLength(191);
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
 
         Paginator::defaultView('bootstrap-4');
 
